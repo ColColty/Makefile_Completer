9aea63083a6431b3c18a2eecb348ffff9e0c0e20
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-06 19:36:53 +0200

    [MODIFY] makefile_completer.py Changing the print option to logging and separate the info messages and the debug messages

--- scripts/adder.py
+++ scripts/adder.py
@@ -2,36 +2,38 @@ import scripts.definers as define
 import scripts.library as lib
 import re
 import os
+import logging
 
 def addFiletoMakefile(realPath, dir_path, identifier):
     makeContent = lib.readWrite(path=dir_path + "Makefile", writing=None)
     line_list = makeContent.splitlines()
 
-    print("++++++ ADDING ++++++\n")
+    logging.info("++++++ ADDING ++++++\n")
     fileName = realPath.replace(dir_path + ((define.IDENTIFIERS[identifier] + '/') if identifier != 2 else ''), '')
-    print("++ The file has de name of: {}".format(fileName))
+    logging.info("++ The file has de name of: {}".format(fileName))
     fileLineMakefile = define.PATHS[identifier] + fileName + "\t\\"
-    print("++ The expected line is: {}\n".format(fileLineMakefile))
+    logging.info("++ The expected line is: {}\n".format(fileLineMakefile))
 
     for line in line_list:
-        print(
+        logging.debug(
             "++ FINDING Makefile Line: {}\t||\tto find -> {}".format(line, fileLineMakefile))
         if re.search(fileLineMakefile + "\\", line):
+            logging.info("The file already exists")
             return
 
     for i, line in enumerate(line_list):
-        print("++ TO ADD Makefile line: {}\t||\tto find -> {}".format(line, fileLineMakefile))
+        logging.debug("++ TO ADD Makefile line: {}\t||\tto find -> {}".format(line, fileLineMakefile))
         if re.match(define.MAKEFILE_VAR[identifier] + define.PATHS[identifier], line) or re.match(define.MAKEFILE_VAR[identifier] + "\t", line):
-            print("++ The line starts here: {}".format(line))
+            logging.debug("++ The line starts here: {}".format(line))
             while line_list[i] != '':
                 i += 1
             line_list.insert(i, fileLineMakefile)
-            print("++ {} was added !\n\n".format(fileLineMakefile))
+            logging.debug("++ {} was added !\n\n".format(fileLineMakefile))
             break
 
     makeContent = '\n'.join(line_list)
     lib.readWrite(path=dir_path + "Makefile", writing=makeContent)
-    print("++ Line well added\n\n")
+    logging.info("++ Line well added\n\n")
 
 if __name__ == '__main__':
     print("This is are the functions to add a file to the Makefile")
--- scripts/remover.py
+++ scripts/remover.py
@@ -1,30 +1,31 @@
 import scripts.library as lib
 import scripts.definers as define
 import re
+import logging
 
 def removeFilefromMakefile(realPath, dir_path, identifier):
     makeContent = lib.readWrite(path=dir_path + "Makefile", writing=None)
     line_list = makeContent.splitlines()
 
-    print("------ REMOVING ------\n")
+    logging.info("------ REMOVING ------\n")
     fileName = realPath.replace(dir_path + ((define.IDENTIFIERS[identifier] + "/") if identifier != 2 else ''), '')
-    print("-- The file name is: {}\n".format(fileName))
+    logging.info("-- The file name is: {}\n".format(fileName))
 
     for line in line_list:
-        print("-- FINDING Makefile Line: {}\t||\tto find -> {}".format(line,
+        logging.debug("-- FINDING Makefile Line: {}\t||\tto find -> {}".format(line,
                                                                        define.PATHS[identifier] + fileName,))
         if define.PATHS[identifier] + fileName in line:
             if re.match(define.MAKEFILE_VAR[identifier] + "\t", line):
-                print("-- Adding the header because we remove it")
+                logging.info("-- Adding the header because we remove it")
                 line_list.insert(line_list.index(
                     line), define.MAKEFILE_VAR[identifier] + "\t\\")
             line_list.remove(line)
-            print("-- {} removed !\n".format(line))
+            logging.debug("-- {} removed !\n".format(line))
             break
 
     makeContent = '\n'.join(line_list)
     lib.readWrite(path=dir_path + "Makefile", writing=makeContent)
-    print("-- Line well removed\n\n")
+    logging.info("-- Line well removed\n\n")
 
 if __name__ == '__main__':
     print("This are the functions to remove a file from a Makefile")
