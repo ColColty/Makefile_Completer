ae8701a9a001bd167f74044e6b9275241026fdfc
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-06 19:36:53 +0200

    [MODIFY] makefile_completer.py Changing the print option to logging and separate the info messages and the debug messages

--- makefile_completer.py
+++ makefile_completer.py
@@ -8,43 +8,44 @@ import scripts.adder as add
 import scripts.remover as rm
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import logging
 
 class FileEvent(FileSystemEventHandler):
     def on_created(self, event):
         if event.src_path[-1] == 'c' and event.src_path[-2] == '.':
             identifier, dir_path = finder.directory_finder(event.src_path)
-            print("++ Path where the Makefile is: {}".format(dir_path))
-            print("++ The file is in {} directory".format(define.PATHS[identifier]))
+            logging.info("++ Path where the Makefile is: {}".format(dir_path))
+            logging.info("++ The file is in {} directory".format(define.PATHS[identifier]))
             if "Makefile" in os.listdir(dir_path):
                 add.addFiletoMakefile(event.src_path, dir_path, identifier)
             else:
-                print("++ Makefile not found in %s" % dir_path)
-            print("\n++++++++ THE FILE WAS ADDED ++++++++\n\n\n")
+                logging.error("++ Makefile not found in %s" % dir_path)
+            logging.info("\n++++++++ THE FILE WAS ADDED ++++++++\n\n\n")
 
     def on_deleted(self, event):
         if event.src_path[-1] == 'c' and event.src_path[-2] == '.':
             identifier, dir_path = finder.directory_finder(event.src_path)
-            print("-- Path where the Makefile is: {}".format(dir_path))
-            print("-- The file is in {} directory".format(define.PATHS[identifier]))
+            logging.info("-- Path where the Makefile is: {}".format(dir_path))
+            logging.info("-- The file is in {} directory".format(define.PATHS[identifier]))
             if "Makefile" in os.listdir(dir_path):
                 rm.removeFilefromMakefile(event.src_path, dir_path, identifier)
             else:
-                print("-- Makefile not found in %s" % dir_path)
-            print("\n-------- THE FILE WAS REMOVED --------\n\n\n")
+                logging.error("-- Makefile not found in %s" % dir_path)
+            logging.info("\n-------- THE FILE WAS REMOVED --------\n\n\n")
 
     def on_moved(self, event):
         if event.src_path[-1] == 'c' and event.src_path[-2] == '.':
-            print("\n=== {} was moved to {} ===\n".format(
+            logging.info("\n=== {} was moved to {} ===\n".format(
                 event.src_path, event.dest_path))
             identifier, dir_path = finder.directory_finder(event.src_path)
-            print("== Path where the Makefile is: {}".format(dir_path))
-            print("== The file is in {} directory".format(define.PATHS[identifier]))
+            logging.info("== Path where the Makefile is: {}".format(dir_path))
+            logging.info("== The file is in {} directory".format(define.PATHS[identifier]))
             if "Makefile" in os.listdir(dir_path):
                 rm.removeFilefromMakefile(event.src_path, dir_path, identifier)
                 add.addFiletoMakefile(event.dest_path, dir_path, identifier)
             else:
-                print("== Makefile not found in %s" % dir_path)
-            print("\n======== THE FILE WAS MOVED =========\n\n\n")
+                logging.error("== Makefile not found in %s" % dir_path)
+            logging.info("\n======== THE FILE WAS MOVED =========\n\n\n")
 
 
 if __name__ == '__main__':
@@ -53,6 +54,7 @@ if __name__ == '__main__':
     # observer.schedule(event_handler, path="/home/tforne/Documents/Epitech/Porfolio/Makefile_Completer/testing", recursive=True)
     observer.schedule(event_handler, path="/home", recursive=True)
     observer.start()
+    logging.basicConfig(level=logging.DEBUG)
 
     try:
         while True:
