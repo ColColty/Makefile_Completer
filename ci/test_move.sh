7272996533e54159e9c49788b1fc182252153461
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-20 19:07:25 +0200

    [MODIFY] test_move.sh Reformat tests for test_move.sh

--- ci/test_move.sh
+++ ci/test_move.sh
@@ -1,172 +1,77 @@
 #!/bin/bash
 
+# ========= functions ============
+
+function move_test() {
+    touch testing/$6/$1
+    mkdir $2
+
+    sleep 4
+
+    mv testing/$6/$1 $2/$1
+
+    sleep 4
+
+    python3 ./ci/tests/find_file.py $3 $4 $5
+
+    if [ $? -ne 0 ]; then
+        echo -e "$1 NOT FOUND"
+        exit 84
+    fi
+
+    python3 ./ci/tests/find_file.py $3 $4 "/$1"
+
+    if [ $? -ne 84 ]; then
+        echo -e "$1 NEED TO BE REMOVED AFTER MOVE"
+        exit 84
+    else
+        echo -e "\033[1;32m$1 PASSED\033[0m"
+    fi
+
+    rm $2/$1
+    rm -r $2
+}
+
+function move_to_root() {
+    touch $1
+
+    sleep 4
+
+    mv $1 $2
+
+    sleep 4
+
+    python3 ./ci/tests/find_file.c $3 $4 $5
+
+    if [ $? -ne 0 ]; then
+        echo -e "When a file is moved, the names has to change"
+        exit 84
+    fi
+
+    python3 ./ci/tests/find_file.c $3 $4 $6
+
+    if [ $? -ne 84 ]; then
+        echo -e "When a file is moved the name has to change"
+        exit 84
+    fi
+
+    rm $2
+}
+
 echo -e "\033[1mTESTING when we move a file from src to src/folder \033[0m"
-
-touch testing/src/file.c
-mkdir testing/src/folder
-
-sleep 4
-
-mv testing/src/file.c testing/src/folder/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/" "SRC\t=" "/folder/file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/" "SRC\t=" "/file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/src/folder/file.c
-
-echo -e "\033[1;32mTest 1 passed\033[0m"
+move_test file.c testing/src/folder "./testing/" "SRC\t=" "/folder/file.c" src
 
 echo -e "\033[1mTESTING when we move a file from src/folder to src \033[0m"
-
-touch testing/src/folder/file.c
-
-sleep 4
-
-mv testing/src/folder/file.c testing/src/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/" "SRC\t=" "/file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/" "SRC\t=" "/folder/file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/src/file.c
-
-echo -e "\033[1;32mTest 2 passed\033[0m"
+move_to_root testing/src/folder/file.c testing/src/file.c "./testing/" "SRC\t=" "/file.c" "/folder/file.c"
 
 echo -e "\033[1mTESTING when we move a file from tests to tests/folder \033[0m"
-
-touch testing/tests/file.c
-mkdir testing/tests/folder
-
-sleep 4
-
-mv testing/tests/file.c testing/tests/folder/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/" "TESTS\t=" "/folder/file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/" "TESTS\t=" "/file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/tests/folder/file.c
-
-echo -e "\033[1;32mTest 3 passed\033[0m"
+move_test file.c testing/tests/folder "./testing/" "TESTS\t=" "/folder/file.c" tests
 
 echo -e "\033[1mTESTING when we move a file from tests/folder to tests \033[0m"
-
-touch testing/tests/folder/file.c
-
-sleep 4
-
-mv testing/tests/folder/file.c testing/tests/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/" "TESTS\t=" "/file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/" "TESTS\t=" "/folder/file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/tests/file.c
-
-echo -e "\033[1;32mTest 4 passed\033[0m"
+move_to_root testing/tests/folder/file.c testing/tests/file.c "./testing/" "TESTS\t=" "/file.c" "/folder/file.c"
 
 echo -e "\033[1mTESTING when we move a file from lib/my to lib/my/folder \033[0m"
-
-touch testing/lib/my/file.c
-mkdir testing/lib/my/folder
-
-sleep 4
-
-mv testing/lib/my/file.c testing/lib/my/folder/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/lib/my/" "SRC\t=" "folder/file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/lib/my/" "SRC\t=" "file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/lib/my/folder/file.c
-
-echo -e "\033[1;32mTest 5 passed\033[0m"
+move_test file.c testing/lib/my/folder "./testing/lib/my/" "SRC\t=" "folder/file.c" lib/my
 
 echo -e "\033[1mTESTING when we move a file from lib/my/folder to lib/my \033[0m"
-
-touch testing/lib/my/folder/file.c
-
-sleep 4
-
-mv testing/lib/my/folder/file.c testing/lib/my/file.c
-
-sleep 4
-
-python3 ./ci/tests/find_file.py "./testing/lib/my/" "SRC\t=" "file.c"
-
-if [ $? -ne 0 ]; then
-    echo -e "When a file is moved, the name has to change"
-    exit 84
-fi
-
-python3 ./ci/tests/find_file.py "./testing/lib/my/" "SRC\t=" "folder/file.c"
-
-if [ $? -ne 84 ]; then
-    echo -e "When a file is moved, the old name has to be removed"
-    exit 84
-fi
-
-rm testing/lib/my/file.c
-
-echo -e "\033[1;32mTest 6 passed\033[0m"
+move_to_root testing/lib/my/folder/file.c testing/lib/my/file.c "./testing/lib/my/" "SRC\t=" "file.c" "folder/file.c"
