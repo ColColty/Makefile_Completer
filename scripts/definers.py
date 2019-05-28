a85833e56ce11773925955baea8309b22af680ef
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-28 17:44:31 +0200

    feat: Adding function to save the json data
    Adding function to save the data to static variables

--- scripts/definers.py
+++ scripts/definers.py
@@ -1,7 +1,23 @@
+import logging
+import scripts.library as lib
+
 JSON_FILES = ["data/config.json"]
 
-IDENTIFIERS = ["src", "tests", "my"]
-PATHS = ["\t$(SRC_DIR)/", "\t$(TEST_SRC)/", "\t"]
-MAKEFILE_VAR = ["SRC\t=", "TESTS\t=", "SRC\t="]
+IDENTIFIERS = []
+PATHS = []
+MAKEFILE_VAR = []
+
+
+def load_definers():
+    data = lib.json_loader(JSON_FILES[0])
+    for folder in data:
+        if folder["folder"] == "lib":
+            IDENTIFIERS.append(folder["my"]["folder"])
+            PATHS.append(folder["my"]["path"])
+            MAKEFILE_VAR.append(folder["my"]["variable"])
+        else:
+            IDENTIFIERS.append(folder["folder"])
+            PATHS.append(folder["path"])
+            MAKEFILE_VAR.append(folder["variable"])
 
 # TODO Add here call for functions to read the json files with configurations
